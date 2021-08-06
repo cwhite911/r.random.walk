1ae7f780e61dc4ad31c109888d72c001ae174c43
Corey White <smortopahri@gmail.com>
2021-08-06 12:36:59 -0400

    Added addition unit test for walkers

--- testsuite/test_r_random_walk.py
+++ testsuite/test_r_random_walk.py
@@ -19,6 +19,13 @@
 # Dependencies
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
+from random_walk import (
+    take_step,
+    walker_is_stuck,
+    avoid_boundary,
+    starting_position,
+    out_of_bounds,
+)
 
 # Tests
 class TestRandomWalk(TestCase):
@@ -51,12 +58,76 @@ class TestRandomWalk(TestCase):
         # assertModule is used to call module which we test
         # we expect module to finish successfully
         self.assertModule(
-            "r.random.walk", input="elevation", output=self.random_walk, steps=1000, overwrite=True
+            "r.random.walk",
+            output=self.random_walk,
+            steps=1000,
+            overwrite=True,
+            seed=0,
+            flags="s",
         )
 
         # self.assert(
         #     self.random_walk, "data/random_walk_result", digits=6, precision=1
         # )
+
+    def test_take_step_dir_4(self):
+        """
+        Tests 4 directional step
+        """
+        step = take_step([0, 0], 4, black_list=[])
+        self.assertTrue(
+            step == [1, 0]
+            or step == [0, 1]  # North
+            or step == [-1, 0]  # East
+            or step == [0, -1],  # South  # West
+            f"Step outside of 4 directional bounds, {step}",
+        )
+
+    def test_take_step_dir_8(self):
+        """
+        Tests 8 directional step
+        """
+        step = take_step([0, 0], 8)
+        self.assertTrue(
+            step == [1, 0]
+            or step == [0, 1]  # N
+            or step == [-1, 0]  # E
+            or step == [0, -1]  # S
+            or step == [1, 1]  # W
+            or step == [-1, 1]  # NE
+            or step == [-1, -1]  # SE
+            or step == [1, -1],  # SW  # NW
+            f"Step outside of 8 directional bounds, {step}",
+        )
+
+    def test_take_step_dir_4_blacklist(self):
+        """
+        Tests 4 directional step blacklist
+        """
+        step = take_step([0, 0], 4, black_list=[0, 1, 2])
+        self.assertTrue(
+            step != [1, 0]
+            and step != [0, 1]  # North
+            and step != [-1, 0]  # East
+            and step == [0, -1],  # South  # West
+            f"Used step from blacklist or outside of 4 directional bounds, {step}",
+        )
+
+    def test_walker_is_not_stuck(self):
+        """
+        Test if walker is stuck and should return False the walker is not stuck
+        """
+        tested_directions = [0, 1, 2]
+        output = walker_is_stuck(tested_directions, 4)
+        self.assertFalse(output)
+
+    def test_walker_is_stuck(self):
+        """
+        Test if walker is stuck and should return True the walker is stuck
+        """
+        tested_directions = [0, 1, 2, 3]
+        output = walker_is_stuck(tested_directions, 4)
+        self.assertTrue(output)
 
 
 if __name__ == "__main__":
